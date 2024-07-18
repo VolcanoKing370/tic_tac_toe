52543e53b4943112fd25107f87bed87f200d9156
MK <35180184+VolcanoKing370@users.noreply.github.com>
2024-07-17 23:26:35 -0400

    updated game mechanics

--- main.c
+++ main.c
@@ -16,8 +16,12 @@ void draw_board() {
     }
 }
 
+// This function allows player to make a VALID move
 bool insert(int x, int y) {
-    scanf("%d",&x);
+    printf("%c's turn!\n", player_token);
+    printf("Enter vertical coord: ");
+    scanf("%d", &x);
+    printf("Enter horizontal coord: ");
     scanf("%d", &y);
 
     printf("\n");
@@ -32,6 +36,49 @@ bool insert(int x, int y) {
 }
 
 // TODO: Add a function to check for a win.
+bool check_win(char brd[3][3], char token) {
+    // Horizontal top row
+    if(brd[0][0] == brd[0][1] && brd[0][1] == brd[0][2] && brd[0][0] == token) {
+        printf("%c WINS!\n", player_token);
+        return true;
+    }
+    // Horitontal middle row
+    if(brd[1][0] == brd[1][1] && brd[1][1] == brd[1][2] && brd[1][0] == token) {
+        printf("%c WINS!\n", player_token);
+        return true;
+    }
+    // Horitontal bottom row
+    if(brd[2][0] == brd[2][1] && brd[2][1] == brd[2][2] && brd[2][0] == token) {
+        printf("%c WINS!\n", player_token);
+        return true;
+    }
+    // Vertical left column
+    if(brd[0][0] == brd[1][0] && brd[1][0] == brd[2][0] && brd[0][0] == token) {
+        printf("%c WINS!\n", player_token);
+        return true;
+    }
+    // Vertical middle column
+    if(brd[0][1] == brd[1][1] && brd[1][1] == brd[2][1] && brd[0][1] == token) {
+        printf("%c WINS!\n", player_token);
+        return true;
+    }
+    // Vertical right column
+    if(brd[0][2] == brd[1][2] && brd[1][2] == brd[2][2] && brd[0][2] == token) {
+        printf("%c WINS!\n", player_token);
+        return true;
+    }
+    // Diagonal from top-left to bottom-right
+    if(brd[0][0] == brd[1][1] && brd[1][1] == brd[2][2] && brd[0][0] == token) {
+        printf("%c WINS!\n", player_token);
+        return true;
+    }
+    // Diagonal from top-right to bottom-left
+    if(brd[0][2] == brd[1][1] && brd[1][1] == brd[2][0] && brd[0][2] == token) {
+        printf("%c WINS!\n", player_token);
+        return true;
+    }
+    return false;
+}
 
 int main() {
     int _x, _y;   //coordinates to place tiles
@@ -40,8 +87,17 @@ int main() {
     while(counter < 9) {
         draw_board();
         move = insert(_x, _y);
-        // add a function to check for win here.
         if(move) counter++;
+        // add a function to check for win here.
+        if(check_win(board, player_token)) {
+            break;
+        }
+        if(move) {
+            // switch player_token character after turn
+            player_token = (player_token == 'X') ? 'O' : 'X';
+        }
+        printf("\ncounter: %d\n", counter);
+        
     }
     printf("\nGAME!\n-----\n");
     draw_board();
